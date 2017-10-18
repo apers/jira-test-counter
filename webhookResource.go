d3e60fa4c35a6e0617c607444b7eb74b88143548
Audun Øygard <pers87@gmail.com>
2017-10-18 14:43:25 +0200

    Removed flagged prerequisite

--- webhookResource.go
+++ webhookResource.go
@@ -21,28 +21,28 @@ func webhookHandler(w http.ResponseWriter, r *http.Request) {
 	if buf.Len() == 0 {
 		return
 	}
+
+	fmt.Println(buf)
+
 	event := convertToJiraJson(buf)
-	if event.ChangeLog.hasStatusChange() && event.Issue.isFlagged() {
+	if event.ChangeLog.hasStatusChange() {
 		from, to := event.ChangeLog.getStatusChange()
 		var taskType string
 		if from == CodeReviewCol && to == TestCol {
 			fmt.Println("CodeReview")
 			fmt.Println("PF: ", event.Issue.Key)
-			fmt.Println("Flagged: ", event.Issue.isFlagged())
 			fmt.Println("User: ", event.User.Name)
 
 			taskType = TaskTypeReview
 		} else if from == TestCol && to == DoneCol {
 			fmt.Println("Test")
 			fmt.Println("PF: ", event.Issue.Key)
-			fmt.Println("Flagged: ", event.Issue.isFlagged())
 			fmt.Println("User: ", event.User.Name)
 
 			taskType = TaskTypeTest
 		} else if from == DevelopmentCol && to == CodeReviewCol {
 			fmt.Println("Development")
 			fmt.Println("PF: ", event.Issue.Key)
-			fmt.Println("Flagged: ", event.Issue.isFlagged())
 			fmt.Println("User: ", event.User.Name)
 
 			taskType = TaskTypeDev
