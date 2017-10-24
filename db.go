589b20eb643945d83f24b9d750f2c3c1272867d4
Audun Pers <pers87@gmail.com>
2017-10-24 20:05:14 +0200

    Fixed botched intellij commit #2

--- db.go
+++ db.go
@@ -13,11 +13,11 @@ import (
 
 /* objects */
 
-type jiradb struct {
+type JiraDb struct {
 	db *sql.DB
 }
 
-type user struct {
+type User struct {
 	username        string
 	email           string
 	availableblocks int
@@ -25,28 +25,28 @@ type user struct {
 
 /* db connections */
 
-func dbconnect() jiradb {
+func dbconnect() JiraDb {
 	db, err := sql.Open("postgres", "user=jira dbname=jira password=jira")
 	check(err)
 	err = db.Ping()
 	check(err)
 
-	var jiradb jiradb
-	jiradb.db = db
+	var JiraDb JiraDb
+	JiraDb.db = db
 
-	return jiradb
+	return JiraDb
 }
 
 /* tasks */
 
-func (db jiradb) addTask(username string, tasktype string, key string, summary string) {
+func (db JiraDb) addTask(username string, tasktype string, key string, summary string) {
 	stmt, err := db.db.Prepare("insert into tasks(username, type, key, summary, time) values($1, $2, $3, $4, $5)")
 	check(err)
 	_, err = stmt.Exec(username, tasktype, key, summary, time.Now())
 	check(err)
 }
 
-func (db jiradb) addToAvailableBlocks(username string, tasktype string) {
+func (db JiraDb) addToAvailableBlocks(username string, tasktype string) {
 
 	blockcount := 0
 
@@ -64,7 +64,7 @@ func (db jiradb) addToAvailableBlocks(username string, tasktype string) {
 	check(err)
 }
 
-func (db jiradb) getAllTaskCount() *sql.Rows {
+func (db JiraDb) getAllTaskCount() *sql.Rows {
 	stmt, err := db.db.Prepare("select username, available_blocks from users")
 	defer stmt.Close()
 	check(err)
@@ -75,7 +75,7 @@ func (db jiradb) getAllTaskCount() *sql.Rows {
 
 /* user*/
 
-func (db jiradb) createUser(username string, email string) {
+func (db JiraDb) createUser(username string, email string) {
 	fmt.Println("creating user: ", username)
 	stmt, err := db.db.Prepare("insert into users(username, email) values($1, $2)")
 	defer stmt.Close()
@@ -84,9 +84,9 @@ func (db jiradb) createUser(username string, email string) {
 	check(err)
 }
 
-func (db jiradb) getuser(username string) (error, user) {
-	var user user
-	err := db.db.QueryRow("select username, email, available_blocks from users where username=$1", username).scan(&user.username, &user.email, &user.availableblocks)
+func (db JiraDb) getuser(username string) (error, User) {
+	var user User
+	err := db.db.QueryRow("select username, email, available_blocks from users where username=$1", username).Scan(&user.username, &user.email, &user.availableblocks)
 
 	if err != nil && err != sql.ErrNoRows {
 		check(err)
@@ -95,7 +95,7 @@ func (db jiradb) getuser(username string) (error, user) {
 	return err, user
 }
 
-func (db jiradb) updateAvailableBlocks(username string, availableblocks int) {
+func (db JiraDb) updateAvailableBlocks(username string, availableblocks int) {
 	fmt.Println("updating available blocks for user: ", username)
 	stmt, err := db.db.Prepare("update users set available_blocks = $1 where username = $2")
 	defer stmt.Close()
@@ -104,7 +104,7 @@ func (db jiradb) updateAvailableBlocks(username string, availableblocks int) {
 	check(err)
 }
 
-func (db jiradb) getUserStats(username string) (error, int) {
+func (db JiraDb) getUserStats(username string) (error, int) {
 	var taskcount int
 	err := db.db.QueryRow("select count(*) from tasks where username=$1", username).Scan(&taskcount)
 
@@ -117,7 +117,7 @@ func (db jiradb) getUserStats(username string) (error, int) {
 
 /* tables and migrations */
 
-func (db jiradb) cleanTables() {
+func (db JiraDb) cleanTables() {
 	fmt.Println("cleaning tables..")
 
 	_, err := db.db.Exec("drop table tasks")
@@ -130,7 +130,7 @@ func (db jiradb) cleanTables() {
 	}
 }
 
-func (db jiradb) initTables() {
+func (db JiraDb) initTables() {
 	log.Print("initializing tables..")
 
 	_, err := db.db.Exec(`
@@ -203,7 +203,7 @@ type migrationEntry struct {
   ]
 }
 */
-func (db jiradb) migrate() {
+func (db JiraDb) migrate() {
 	log.Print("migrating..")
 
 	var migrations migrations
